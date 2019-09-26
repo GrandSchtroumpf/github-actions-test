c54890c940ffc566d2c3cd2c314ae562210743cb
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-09-26 15:59:14 +0200

    add 2 token

--- .github/actions/build/index.ts
+++ .github/actions/build/index.ts
@@ -4,13 +4,14 @@ import { context, GitHub } from '@actions/github';
 async function createFile() {
   try {
     const token = getInput('GITHUB_TOKEN');
+    const myToken = getInput('MY_TOKEN');
     debug('Inside try block');
   
-    if (!token) {
-      warning(`Token with value ${token} is not provided`);
+    if (!token || myToken) {
+      warning(`Github with value ${token} and Mytoken with value ${myToken} is not provided`);
       throw new Error('Cannot find token');
     } else {
-      const octokit = new GitHub(token);
+      const octokit = new GitHub(token || myToken);
       const { data } = await octokit.repos.createOrUpdateFile({
         ...context.repo,
         content: 'Hello World',
